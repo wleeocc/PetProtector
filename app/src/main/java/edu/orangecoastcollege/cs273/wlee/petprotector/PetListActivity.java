0c01c3ec7d50fc79ce84cd252ceac8166d6f24b1
Wonseop <wonseop.s.lee@gmail.com>
2016-10-25 16:40:24 -0700

    final check ImageView

--- app/src/main/java/edu/orangecoastcollege/cs273/wlee/petprotector/PetListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/wlee/petprotector/PetListActivity.java
@@ -1,13 +1,111 @@
 package edu.orangecoastcollege.cs273.wlee.petprotector;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.AnyRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class PetListActivity extends AppCompatActivity {
+
+    private ImageView petImageView;
+
+    // This member variable stores the URI to whatever image has been selected
+    // Default: none.png (R.drawable.none)
+    private Uri imageURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_list);
+
+        // Hook up the petImageView to the Layout:
+        petImageView = (ImageView) findViewById(R.id.petImageView);
+
+        // This constructs a full URI to any Android resource (id, drawable, color, etc.)
+        imageURI = getUriToResource(this, R.drawable.none);
+
+        // Set the imageURI of the ImageView in code
+        petImageView.setImageURI(imageURI);
+    }
+
+
+    public void selectPetImage(View view){
+
+        // List of all the permissions we need to request from user
+        ArrayList<String> permList = new ArrayList<>();
+
+        // Start by seeing if we have permission to camera
+        int cameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED)
+            permList.add(Manifest.permission.CAMERA);
+
+        // Next check to see if we have read external storage permission
+        int readExternalStoragePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (readExternalStoragePermission != PackageManager.PERMISSION_GRANTED)
+            permList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        int writeExternalStoragePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (writeExternalStoragePermission != PackageManager.PERMISSION_GRANTED)
+            permList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        int requestCode = 100; // if can access, returns 100
+        // If the list has items (size > 0), we need to request permissions from the user
+        if (permList.size() > 0)
+        {
+            // Convert the ArrayList into an Array of Strings
+            String[] perms = new String[permList.size()];
+            // Request permissions from the user:
+            // request code was here
+            ActivityCompat.requestPermissions(this, permList.toArray(perms), requestCode);
+        }
+
+        // If we have all 3 permissions, open ImageGallery:
+        if (cameraPermission == PackageManager.PERMISSION_GRANTED
+            && readExternalStoragePermission == PackageManager.PERMISSION_GRANTED
+            && writeExternalStoragePermission == PackageManager.PERMISSION_GRANTED)
+        {
+            // Use an Intent to launch gallery and take pictures
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent, requestCode);
+        }
+        else
+            Toast.makeText(this,
+                    "Pet Protector requires camera and external storage permission",
+                    Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * Get Uri to any resource type within an Android Studio project. Method is public static
+     * to allow other classes to use it as a helper function.
+     *
+     * @param context The current context
+     * @param resId The resource identifier of the drawable
+     * @return Uri to resource by given id
+     * @throws Resources.NotFoundException If the given resource id does not exist.
+     */
+    public static Uri getUriToResource(@NonNull Context context, @AnyRes int resId) throws Resources.NotFoundException {
+        /** Return a Resource instance for your application's package. */
+        Resources res = context.getResources();
+        /** return URI */
+        return Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE +
+                "://" + res.getResourceName(resId)
+                + '/' + res.getResourceTypeName(resId)
+                + '/' + res.getResourceEntryName(resId));
+
     }
 }
